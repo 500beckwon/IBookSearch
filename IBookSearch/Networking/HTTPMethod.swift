c6cfad6fc62defecd1ddebebfd98906e757122e6
500beckwon <kjsx60@gmail.com>
2022-10-31 23:03:19 +0900

    HTTPMethod 제작

---
+++ IBookSearch/Networking/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  IBookSearch
+//
+//  Created by ByungHoon Ann on 2022/10/31.
+//
+
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
