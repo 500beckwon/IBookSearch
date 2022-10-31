4158434e455830bba387abf1d3e7c53af49d9eac
500beckwon <kjsx60@gmail.com>
2022-10-31 22:59:18 +0900

    Scene File 이동

--- IBookSearch/Scene/SceneDelegate.swift
+++ IBookSearch/Scene/SceneDelegate.swift
@@ -15,12 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = ViewController()
+        let mainViewController = BookListViewController()
         window?.windowScene = windowScene
-        window?.backgroundColor = .systemBackground
         window?.rootViewController = UINavigationController(rootViewController: mainViewController)
         window?.makeKeyAndVisible()
     }
