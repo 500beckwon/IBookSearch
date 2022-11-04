d5f53e27feb869a6809105bd1759277411644d10
500beckwon <kjsx60@gmail.com>
2022-11-04 22:40:57 +0900

    상세보기 전용 UILabel 제작

--- IBookSearch/Presentation/BookDetail/PaddingLabel.swift
+++ IBookSearch/Presentation/BookDetail/PaddingLabel.swift
@@ -5,4 +5,28 @@
 //  Created by ByungHoon Ann on 2022/11/04.
 //
 
-import Foundation
+import UIKit
+
+final class PaddingLabel: UILabel {
+    
+    var topInset: CGFloat = 10.0
+    var bottomInset: CGFloat = 5.0
+    var leftInset: CGFloat = 8.0
+    var rightInset: CGFloat = 8.0
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset,
+                                  left: leftInset,
+                                  bottom: bottomInset,
+                                  right: rightInset)
+        
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        let width = size.width + leftInset + rightInset
+        let height = size.height + topInset + bottomInset
+        return CGSize(width: width, height: height)
+    }
+}
