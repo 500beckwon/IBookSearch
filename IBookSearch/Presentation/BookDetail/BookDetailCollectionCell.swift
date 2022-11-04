1324213324fc8d62542013fa4aea67baeafac953
500beckwon <kjsx60@gmail.com>
2022-11-04 12:51:43 +0900

    BookDetail UI

--- IBookSearch/Presentation/BookDetail/BookDetailCollectionCell.swift
+++ IBookSearch/Presentation/BookDetail/BookDetailCollectionCell.swift
@@ -5,4 +5,8 @@
 //  Created by ByungHoon Ann on 2022/11/04.
 //
 
-import Foundation
+import UIKit
+
+final class BookDetailCollectionCell: UICollectionViewCell {
+    
+}
--- IBookSearch/Presentation/BookDetail/BookDetailViewController.swift
+++ IBookSearch/Presentation/BookDetail/BookDetailViewController.swift
@@ -1,8 +1,12 @@
 //
-//  BookDetailVIewController.swift
+//  BookDetailViewController.swift
 //  IBookSearch
 //
 //  Created by ByungHoon Ann on 2022/11/04.
 //
 
-import Foundation
+import UIKit
+
+final class BookDetailViewController: UIViewController {
+    
+}
