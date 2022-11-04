ef42d416b511aa039c24f906efa61b5e7269e9db
500beckwon <kjsx60@gmail.com>
2022-11-04 12:51:33 +0900

    CollectionView Reuse Extension

--- IBookSearch/Extension/UICollectionView+Extension.swift
+++ IBookSearch/Extension/UICollectionView+Extension.swift
@@ -5,4 +5,19 @@
 //  Created by ByungHoon Ann on 2022/11/04.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    func registerCell<T: UICollectionViewCell>(_ cell: T.Type) {
+        register(cell.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard
+            let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier,
+                                           for: indexPath) as? T else {
+            fatalError("Unable to dequeue Reusable UICollectionViewCell")
+        }
+        return cell
+    }
+}
--- IBookSearch/Extension/UICollectionViewCell+Extension.swift
+++ IBookSearch/Extension/UICollectionViewCell+Extension.swift
@@ -5,4 +5,6 @@
 //  Created by ByungHoon Ann on 2022/11/04.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionViewCell: Reusable { }
