ec6392ab5d895d93042fa1f1294903fd3db3d18c
500beckwon <kjsx60@gmail.com>
2022-11-06 03:33:44 +0900

    NSMutableAttributedString Bold Extension

--- IBookSearch/Extension/NSMutableAttributedString.swift
+++ IBookSearch/Extension/NSMutableAttributedString.swift
@@ -5,4 +5,13 @@
 //  Created by ByungHoon Ann on 2022/11/06.
 //
 
-import Foundation
+import UIKit
+
+extension NSMutableAttributedString {
+    func bold(string: String, fontSize: CGFloat) -> NSMutableAttributedString {
+        let font = UIFont.boldSystemFont(ofSize: fontSize)
+        let attributes: [NSAttributedString.Key: Any] = [.font: font]
+        self.append(NSAttributedString(string: string, attributes: attributes))
+        return self
+    }
+}
