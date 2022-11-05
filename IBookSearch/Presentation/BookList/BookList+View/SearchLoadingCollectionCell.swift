9ccaf606c63caedfeb681e9cd46de116b0d540e9
500beckwon <kjsx60@gmail.com>
2022-11-06 03:35:56 +0900

    UICollectionViewCell 제작

--- IBookSearch/Presentation/BookList/BookList+View/SearchLoadingCollectionCell.swift
+++ IBookSearch/Presentation/BookList/BookList+View/SearchLoadingCollectionCell.swift
@@ -5,4 +5,26 @@
 //  Created by ByungHoon Ann on 2022/11/05.
 //
 
-import Foundation
+import UIKit
+
+final class SearchLoadingCollectionCell: UICollectionViewCell {
+    private let indicator = UIActivityIndicatorView()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        contentView.addSubview(indicator)
+        
+        indicator.hidesWhenStopped = true
+        
+        indicator.anchor(centerX: contentView.centerXAnchor,
+                         centerY: contentView.centerYAnchor)
+    }
+    
+    func startLoading() {
+        indicator.startAnimating()
+    }
+}
--- IBookSearch/Presentation/CustomUI/BaseCollectionViewCell.swift
+++ IBookSearch/Presentation/CustomUI/BaseCollectionViewCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
-class BaseCollectionViewCell: UICollectionViewCell {
+public class BaseCollectionViewCell: UICollectionViewCell {
+    
     var bookImageView = UIImageView()
 }
