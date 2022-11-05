2c3281f970653a7b771cbc346308bf5f4f20737f
500beckwon <kjsx60@gmail.com>
2022-11-06 03:32:01 +0900

    CG Helper Extension

--- IBookSearch/Extension/CGFloat+Extension.swift
+++ IBookSearch/Extension/CGFloat+Extension.swift
@@ -1,5 +1,5 @@
 //
-//  Extension+CGFloat.swift
+//  CGFloat+Extension.swift
 //  IBookSearch
 //
 //  Created by ByungHoon Ann on 2022/11/05.
@@ -8,14 +8,10 @@
 import UIKit
 
 extension CGFloat {
-    static func scaleAndShift(
-        value: CGFloat,
-        inRange: (min: CGFloat, max: CGFloat),
-        toRange: (min: CGFloat, max: CGFloat) = (min: 0.0, max: 1.0)
-    ) -> CGFloat {
-        assert(inRange.max > inRange.min)
-        assert(toRange.max > toRange.min)
-        
+    static func scaleAndShift(value: CGFloat,
+                              inRange: (min: CGFloat,
+                                        max: CGFloat),
+                              toRange: (min: CGFloat, max: CGFloat) = (min: 0.0, max: 1.0)) -> CGFloat {
         if value < inRange.min {
             return toRange.min
         } else if value > inRange.max {
--- IBookSearch/Extension/CGSize+Extension.swift
+++ IBookSearch/Extension/CGSize+Extension.swift
@@ -5,4 +5,11 @@
 //  Created by ByungHoon Ann on 2022/11/05.
 //
 
-import Foundation
+import UIKit
+
+extension CGSize {
+    var pixelSize: CGSize {
+        let scale = UIScreen.main.scale
+        return CGSize(width: width * scale, height: height * scale)
+    }
+}
