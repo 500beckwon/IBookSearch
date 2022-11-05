068e904cb9cb4e01f11d77f06fc293aea7d31e5b
500beckwon <kjsx60@gmail.com>
2022-11-06 03:37:14 +0900

    SceneDelegate 코드 수정

--- IBookSearch/Scene/SceneDelegate.swift
+++ IBookSearch/Scene/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
-    
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
@@ -19,9 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         let mainViewController = BookListViewController()
         window?.windowScene = windowScene
-        window?.rootViewController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = SelectAnimationNavigationViewController(rootViewController: mainViewController)
         window?.makeKeyAndVisible()
     }
-    
 }
 
