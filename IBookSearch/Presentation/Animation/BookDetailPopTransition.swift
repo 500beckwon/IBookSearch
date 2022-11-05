4bbd5eabcb920b8178822d1425e1f59dd48cf74d
500beckwon <kjsx60@gmail.com>
2022-11-06 03:36:38 +0900

    NavigationViewController Animation 분할 및 코드 수정

--- IBookSearch/Presentation/Animation/BookDetailPopTransition.swift
+++ IBookSearch/Presentation/Animation/BookDetailPopTransition.swift
@@ -5,4 +5,84 @@
 //  Created by ByungHoon Ann on 2022/11/06.
 //
 
-import Foundation
+import UIKit
+
+public class BookDetailPopTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    fileprivate let toDelegate: BookDetailTransitionAnimatorDelegate
+    fileprivate let bookDetailVC: BookDetailViewController
+    
+    fileprivate let transitionImageView = TransitionImageView()
+    
+    init?(
+        toDelegate: Any,
+        fromBookDetailVC bookDetailVC: BookDetailViewController
+    ) {
+        guard let toDelegate = toDelegate as? BookDetailTransitionAnimatorDelegate else {
+            return nil
+        }
+        
+        self.toDelegate = toDelegate
+        self.bookDetailVC = bookDetailVC
+    }
+    
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.38
+    }
+    
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let fromView = transitionContext.view(forKey: .from)
+        let toView = transitionContext.view(forKey: .to)
+        let containerView = transitionContext.containerView
+        let fromReferenceFrame = bookDetailVC.imageFrame() ?? .zero
+        
+        let transitionImage = bookDetailVC.referenceImage()
+        transitionImageView.image = transitionImage
+        transitionImageView.frame = bookDetailVC.imageFrame() ?? .zero
+        
+        [toView, fromView]
+            .compactMap { $0 }
+            .forEach { containerView.addSubview($0) }
+        containerView.addSubview(transitionImageView)
+        
+        self.bookDetailVC.transitionWillStart()
+        self.toDelegate.transitionWillStart()
+        
+        let duration = self.transitionDuration(using: transitionContext)
+        let spring: CGFloat = 0.9
+        let animator = UIViewPropertyAnimator(duration: duration, dampingRatio: spring) {
+            fromView?.alpha = 0
+        }
+        
+        animator.addCompletion { [weak self] position in
+            guard let self = self else { return }
+            self.transitionImageView.removeFromSuperview()
+            self.transitionImageView.image = nil
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            self.toDelegate.transitionDidEnd()
+            self.bookDetailVC.transitionDidEnd()
+        }
+        animator.startAnimation()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+            animator.addAnimations { [weak self] in
+                guard let self = self else { return }
+                let toReferenceFrame = self.toDelegate.imageFrame() ??
+                BookDetailPopTransition.defaultOffscreenFrameForDismissal(
+                    transitionImageSize: fromReferenceFrame.size,
+                    screenHeight: containerView.bounds.height
+                )
+                self.transitionImageView.frame = toReferenceFrame
+            }
+        }
+    }
+    
+    public static func defaultOffscreenFrameForDismissal(transitionImageSize: CGSize,
+                                                         screenHeight: CGFloat) -> CGRect {
+        return CGRect(
+            x: 0,
+            y: screenHeight,
+            width: transitionImageSize.width,
+            height: transitionImageSize.height
+        )
+    }
+}
--- IBookSearch/Presentation/Animation/BookDetailPushTransition.swift
+++ IBookSearch/Presentation/Animation/BookDetailPushTransition.swift
@@ -5,4 +5,80 @@
 //  Created by ByungHoon Ann on 2022/11/06.
 //
 
-import Foundation
+import UIKit
+
+final public class BookDetailPushTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    fileprivate let fromDelegate: BookDetailTransitionAnimatorDelegate
+    fileprivate let bookDetailVC: BookDetailViewController
+    fileprivate let transitionImageView = TransitionImageView()
+    
+    init?(fromDelegate: Any,
+          toBookDetailVC bookDetailVC: BookDetailViewController) {
+        guard let fromDelegate = fromDelegate as? BookDetailTransitionAnimatorDelegate else {
+            return nil
+        }
+        self.fromDelegate = fromDelegate
+        self.bookDetailVC = bookDetailVC
+    }
+    
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.38
+    }
+    
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let toView = transitionContext.view(forKey: .to)
+        let fromView = transitionContext.view(forKey: .from)
+        
+        let containerView = transitionContext.containerView
+        toView?.alpha = 0
+        
+        [fromView, toView]
+            .compactMap { $0 }
+            .forEach { containerView.addSubview($0) }
+        
+        let transitionImage = fromDelegate.referenceImage() ?? UIImage()
+        transitionImageView.image = transitionImage
+        containerView.addSubview(transitionImageView)
+        
+        transitionImageView.frame = fromDelegate.imageFrame() ?? BookDetailPushTransition.defaultOffscreenFrameForPresentation(image: transitionImage, forView: toView)
+        let toReferenceFrame = BookDetailPushTransition.calculateZoomInImageFrame(image: transitionImage, forView: toView)
+        
+        fromDelegate.transitionWillStart()
+        bookDetailVC.transitionWillStart()
+        
+        let duration = transitionDuration(using: transitionContext)
+        let spring: CGFloat = 0.95
+        
+        let animator = UIViewPropertyAnimator(duration: duration, dampingRatio: spring) { [weak self] in
+            self?.transitionImageView.frame = toReferenceFrame
+            toView?.alpha = 1
+        }
+        
+        animator.addCompletion { [weak self] (position) in
+            guard let self = self else { return }
+            self.transitionImageView.removeFromSuperview()
+            self.transitionImageView.image = nil
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            self.bookDetailVC.transitionDidEnd()
+            self.fromDelegate.transitionDidEnd()
+        }
+        
+        animator.startAnimation()
+    }
+    
+    private static func defaultOffscreenFrameForPresentation(image: UIImage,
+                                                             forView view: UIView?) -> CGRect {
+        var result = BookDetailPushTransition.calculateZoomInImageFrame(image: image, forView: view)
+        result.origin.y = view?.bounds.height ?? 0
+        return result
+    }
+    
+    private static func calculateZoomInImageFrame(image: UIImage,
+                                                  forView view: UIView?) -> CGRect {
+        let width = UIScreen.main.bounds.width
+        let size = CGSize(width: width, height: width)
+        var rect = CGRect.makeRect(aspectRatio: size, insideRect: view?.bounds ?? .zero)
+        rect.origin.y = view?.safeAreaInsets.top ?? 0
+        return rect
+    }
+}
