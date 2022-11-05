9991222ea2690d221bb9337f6363ce0859aab238
500beckwon <kjsx60@gmail.com>
2022-11-06 03:33:02 +0900

    AutoLayout 함수 indent

--- IBookSearch/Extension/UIView+Extension.swift
+++ IBookSearch/Extension/UIView+Extension.swift
@@ -22,11 +22,13 @@ extension UIView {
                 centerYConstant: CGFloat = 0,
                 width: CGFloat = 0,
                 height: CGFloat = 0) {
+        
         translatesAutoresizingMaskIntoConstraints = false
+        
         if let top = top {
             topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
         }
-      
+        
         if let bottom = bottom {
             bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
         }
